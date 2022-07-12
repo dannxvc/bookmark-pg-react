1aeb70b01f1908ca3101bcc6453774f47726168d
danna <dannavila.1809@gmail.com>
2022-07-12 00:07:01 -0500

    Configure main styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-
+import './App.css';
 function App() {
     return ( 
-        <>
+        <div className="App">
             <Header></Header>
             <Main></Main>
             <Footer></Footer>
-        </>      
+        </div>      
      );
 }
 
